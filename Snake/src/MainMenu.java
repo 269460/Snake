1a00da5544a5788f805f3459017be319146b9412
jacekraczkiewicz5 <163914742+jacekraczkiewicz5@users.noreply.github.com>
2024-06-13 18:56:14 +0200

    Update MainMenu.java

--- Snake/src/MainMenu.java
+++ Snake/src/MainMenu.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class MainMenu extends JPanel {
     public enum Mode {
@@ -25,9 +26,11 @@ public class MainMenu extends JPanel {
 
         JButton singlePlayerButton = new JButton("Single Player");
         JButton playWithComputerButton = new JButton("Play with Computer");
+        JButton highscoreButton = new JButton("Highscore");
 
         singlePlayerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         playWithComputerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        highscoreButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         singlePlayerButton.addActionListener(new ActionListener() {
             @Override
@@ -45,25 +48,46 @@ public class MainMenu extends JPanel {
             }
         });
 
+        highscoreButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showHighscore();
+            }
+        });
+
         add(Box.createRigidArea(new Dimension(0, 50)));
         add(title);
         add(Box.createRigidArea(new Dimension(0, 50)));
         add(singlePlayerButton);
         add(Box.createRigidArea(new Dimension(0, 20)));
         add(playWithComputerButton);
+        add(Box.createRigidArea(new Dimension(0, 20)));
+        add(highscoreButton);
     }
 
     private void startGame() {
         if (selectedMode == Mode.SINGLE_PLAYER) {
-            Normal game = new Normal();
+            Normal game = new Normal(parentFrame);
             parentFrame.setContentPane(game);
             parentFrame.revalidate();
             game.requestFocusInWindow();
         } else if (selectedMode == Mode.PLAY_WITH_COMPUTER) {
-            SnakeGame game = new SnakeGame(true);
+            SnakeGame game = new SnakeGame(true, parentFrame);
             parentFrame.setContentPane(game);
             parentFrame.revalidate();
             game.requestFocusInWindow();
+        }
+    }
+
+    private void showHighscore() {
+        try {
+            int singlePlayerHighscore = Highscore.readSinglePlayerHighscore();
+            int playWithComputerHighscore = Highscore.readPlayWithComputerHighscore();
+            String message = String.format("Single Player:    %d%nwith Computer:  %d",
+                    singlePlayerHighscore, playWithComputerHighscore);
+            JOptionPane.showMessageDialog(parentFrame, message, "Highscore", JOptionPane.INFORMATION_MESSAGE);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(parentFrame, "Failed to read highscore.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
